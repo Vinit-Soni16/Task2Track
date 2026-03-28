bac3ed8ed90c06cb82eef0532571e4be04455d74
Vinit-Soni16 <israniprincy@gmail.com>
2026-03-28 20:45:43 +0530

    update

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -1,10 +1,9 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST || 'smtp.gmail.com',
-  port: parseInt(process.env.SMTP_PORT),
-  
-  secure: true,
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false, // ⚠️ must be false
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
