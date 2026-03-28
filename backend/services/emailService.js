5c576bb8f5d3e31852b7895ac6423798ddbe36d2
Vinit-Soni16 <israniprincy@gmail.com>
2026-03-28 18:27:14 +0530

    in

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -3,6 +3,7 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST || 'smtp.gmail.com',
   port: parseInt(process.env.SMTP_PORT),
+  
   secure: true,
   auth: {
     user: process.env.SMTP_USER,
